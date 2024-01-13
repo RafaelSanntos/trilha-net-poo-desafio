946aca65a14f14708bbf6d9862e200315be09c61
Rafael Santos <rafaelgamail.9@gmail.com>
2024-01-13 11:06:14 -0300

    Desafio Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Exemplo de uso da classe Iphone
+  Console.WriteLine("Celular Iphone:");
+  Iphone iphone = new Iphone(numero: "907129958", modelo: "iPhone 15", imei: "IMEI907", memoria: 128);
+  iphone.Ligar();
+  iphone.InstalarAplicativo("WhatsApp");
+
+  Console.WriteLine();
+
+  // Exemplo de uso da classe Nokia
+  Console.WriteLine("Celular Nokia:");
+  Nokia nokia = new Nokia(numero: "568535434", modelo: "Nokia 3310", imei: "IMEI326", memoria: 36);
+  nokia.ReceberLigacao();
+  nokia.InstalarAplicativo("Snake");
+
+  Console.ReadLine();
